15980782ccc87106855006676cbb5e04f361b2ec
adlascio <arthur.dilascio@gmail.com>
2022-05-10 16:26:51 -0700

    added url ex5

--- exercises/ex5.js
+++ exercises/ex5.js
@@ -4,6 +4,8 @@ In this exercise, we will be given a normal string of words and turn it into a p
 Percent Encoding
 Take a look at the following URL, specifically the last part:
 
+https://www.google.com/search?q=cornerstone%20college
+
 This URL will perform a google search for the term "cornerstone college". Notice that when the string "cornerstone college" is part of a URL, the space is replaced with %20.
 
 If we want to add a parameter to a url, there are certain characters that must be encoded in order to make the URL valid. There are many characters that must be encoded, including: , !, ", and #. For this exercise, we will only be focusing on replacing the space with %20.
